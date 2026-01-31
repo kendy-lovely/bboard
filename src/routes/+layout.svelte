15036f0639581710b2775006e26c393bc7add291
unknown <rafaadzandanendra13@gmail.com>
2026-01-31 21:01:51 +0700

    fixed unregistered users not being able to access the website

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,11 +27,11 @@
 <div class="outer">
 	<nav>
 		<a href="/">home</a>
-		{#if userData.username}
+		{#if userData?.username}
 		<form method="POST" use:enhance={() => {
 				invalidate("supabase:auth");
 				return;
-			}}><p>hi <a href="/{userData.username}">{userData.username}</a> ! <button class="link-style-button" formaction="/logout">logout</button></p>
+			}}><p>hi <a href="/{userData?.username}">{userData?.username}</a> ! <button class="link-style-button" formaction="/logout">logout</button></p>
 		</form>
 		{#if form?.error}<p>{form?.message}</p>{/if}
 		{/if}
